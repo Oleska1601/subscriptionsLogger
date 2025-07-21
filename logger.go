7263f0b44c76c3fd5849102d75f06a235caf491f
oleska <oleska1601@gmail.com>
2025-07-21 17:41:01 +0300

    add methods with and without status

--- logger.go
+++ logger.go
@@ -9,9 +9,11 @@ import (
 
 type LoggerInterface interface {
 	Debug(string, string, string, ...any)
-	Info(string, string, int, ...any)
+	InfowithStatus(string, string, int, ...any)
+	Info(string, string, ...any)
 	Warn(string, string, string, ...any)
-	Error(string, string, string, int, error, ...any)
+	ErrorWithStatus(string, string, string, int, error, ...any)
+	Error(string, string, string, error, ...any)
 	Fatal(string, string, string, error, ...any)
 }
 
@@ -33,16 +35,24 @@ func (l *Logger) Debug(funcName string, actionName string, msg string, args ...a
 	l.logger.Debug(msg, append([]any{slog.String("path", funcName+" "+actionName)}, args...)...)
 }
 
-func (l *Logger) Info(funcName string, msg string, status int, args ...any) {
+func (l *Logger) InfowithStatus(funcName string, msg string, status int, args ...any) {
 	l.logger.Info(msg, append([]any{slog.String("path", funcName), slog.Int("status", status)}, args...)...)
+}
+
+func (l *Logger) Info(funcName string, msg string, args ...any) {
+	l.logger.Info(msg, append([]any{slog.String("path", funcName)}, args...)...)
 }
 
 func (l *Logger) Warn(funcName string, actionName string, msg string, args ...any) {
 	l.logger.Warn(msg, append([]any{slog.String("path", funcName+" "+actionName)}, args...)...)
 }
 
-func (l *Logger) Error(funcName string, actionName string, msg string, status int, err error, args ...any) {
+func (l *Logger) ErrorWithStatus(funcName string, actionName string, msg string, status int, err error, args ...any) {
 	l.logger.Error(funcName, append([]any{slog.String("path", funcName+" "+actionName), slog.Int("status", status), slog.Any("error", err)}, args...)...)
+}
+
+func (l *Logger) Error(funcName string, actionName string, msg string, err error, args ...any) {
+	l.logger.Error(funcName, append([]any{slog.String("path", funcName+" "+actionName), slog.Any("error", err)}, args...)...)
 }
 
 func (l *Logger) Fatal(funcName string, actionName string, msg string, err error, args ...any) {
